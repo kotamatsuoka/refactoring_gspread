1a0b0d76e37a9de1ea21b43c40ee64624d576048
k.matsuoka <kota11250910@gmail.com>
2018-08-06 20:36:57 +0900

    Split into the part of preparing spreadsheet and writing on spreadsheet.
    - [ ] メソッドを役割ごとに切り離した
        - [ ] worksheetを準備するメソッド
        - [ ] worksheetに日時とrunning_timeを記録するメソッド

--- main.py
+++ main.py
@@ -5,20 +5,26 @@ import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 
 
-def main(running_time):
-    scope = ['https://spreadsheets.google.com/feeds',
-             'https://www.googleapis.com/auth/drive']
+class Spreadsheet:
+    def prepare_worksheet(self):
+        """worksheetを準備する。"""
+        scope = ['https://spreadsheets.google.com/feeds',
+                 'https://www.googleapis.com/auth/drive']
+        p = Path(__file__)
+        q = p.parent / 'gspread.json'
 
-    p = Path(__file__)
-    q = p.parent / 'gspread.json'
+        credentials = ServiceAccountCredentials.from_json_keyfile_name(q, scope)
+        gc = gspread.authorize(credentials)
 
-    credentials = ServiceAccountCredentials.from_json_keyfile_name(q, scope)
-    gc = gspread.authorize(credentials)
-    worksheet = gc.open('gspreadサンプル').sheet1
+        return gc.open('gspreadサンプル').sheet1
 
-    current_datetime = datetime.datetime.now().strftime('%Y/%m/%d %H:%M:%S')
-    worksheet.append_row([current_datetime, running_time])
+    def main(self, running_time):
+        """準備されたworksheetに、現在の日時とrunning_timeを記録する"""
+        worksheet = self.prepare_worksheet()
+
+        current_datetime = datetime.datetime.now().strftime('%Y/%m/%d %H:%M:%S')
+        worksheet.append_row([current_datetime, running_time])
 
 
 if __name__ == '__main__':
-    main(running_time=20)
+    Spreadsheet().main(running_time=20)
